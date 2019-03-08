bbc2305eed1be113a34ba006e019414d11a10849
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-03-07 20:32:03 -0500

    Update Main.java

--- src/com/ConvoCulture/Main.java
+++ src/com/ConvoCulture/Main.java
@@ -1,9 +1,7 @@
 package com.ConvoCulture;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class Main
